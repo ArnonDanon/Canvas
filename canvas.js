96206fb10fe9ec9326281694f7b26f00562fbe4e
Arnon Danon <arnond78@gmail.com>
2020-07-04 01:46:27 +0300

    Drawing a circle in middle of canvas

--- canvas.js
+++ canvas.js
@@ -1,11 +1,26 @@
 (function (){
     var canvas= document.querySelector("canvas");
-    resizeCanvas(); 
+    var contex2d=canvas.getContext('2d');
 
     function resizeCanvas(){
-    canvas.height=window.innerHeight;
-    canvas.width=window.innerWidth;
+        canvas.height=window.innerHeight;
+        canvas.width=window.innerWidth;
+        drawCircle();
     }
+
+    resizeCanvas();
+    
+    function drawCircle(){
+        contex2d.beginPath();
+        contex2d.arc(
+            canvas.width/2,
+            canvas.height/2,
+            25,
+            0,
+            2*Math.PI);
+        contex2d.stroke();
+    };
+    
 
     window.addEventListener("resize",resizeCanvas) ;
     console.log(canvas);
